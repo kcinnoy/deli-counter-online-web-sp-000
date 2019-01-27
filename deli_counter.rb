1478d135f22432e89711b764974ef1c62662b9cd
Yonnick Brown <25hour24@gmail.com>
2019-01-27 20:33:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,12 @@ def deli_counter
   katz_deli = []
 
   def line(position)
+    puts "The line is currently empty."
 
   end
 
   def take_a_number(name, position)
+    katz_deli << name
     puts "Welcome #{name}. You are #{position} in the line "
   end
 

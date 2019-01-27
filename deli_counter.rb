f349b11379786bac85008e626ae98e5345c9bd41
Yonnick Brown <25hour24@gmail.com>
2019-01-27 21:09:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,12 +5,12 @@ def line(position)
   # line_print = []
   if katz_deli.empty?
     puts "The line is currently empty."
-  else 
+  else
     katz_deli.each_with_index do |name, index|
       msg += "#{index.to_i+1}. #{name}"
     end
     puts msg
-  end 
+  end
 end
 
 # def take_a_number(name, position)

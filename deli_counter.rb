e897ddaf3e5c1c00981ea1ddecef187288668c11
Yonnick Brown <25hour24@gmail.com>
2019-01-27 20:54:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,21 +1,18 @@
-def deli_counter
-  katz_deli = []
 
-  def line(position)
-    line_print = []
-    if katz_deli.empty?
-       puts "The line is currently empty."
-    else katz_deli.each_with_index do |name, index|
-      line_print << "#{index+1}. #{name}"
-    end
-    puts "The line is currently: #{line_print.join(" ")}"
+katz_deli = []
+  
+def line(position)
+  line_print = []
+  if katz_deli.empty?
+     puts "The line is currently empty."
+  else katz_deli.each_with_index do |name, index|
+    line_print << "#{index+1}. #{name}"
   end
-
-  def take_a_number(name, position)
-    katz_deli << name
-    number = katz_deli.size
-    puts "Welcome #{name}. You are #{number} in the line "
-  end
-
+  puts "The line is currently: #{line_print.join(" ")}"
 end
+
+def take_a_number(name, position)
+  katz_deli << name
+  number = katz_deli.size
+  puts "Welcome #{name}. You are #{number} in the line "
 end
